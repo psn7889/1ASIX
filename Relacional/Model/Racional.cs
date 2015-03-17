e32e81197c696a6c3916085a27268b39df1a4a19
psn7889 <poison7889@hotmail.com>
2015-03-17 19:57:01 +0100

    primero

---
+++ Relacional/Model/Racional.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relacional.Model
+{
+    class Racional
+    {
+        private int a;
+        private int b;
+
+
+        public Racional(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int getA()
+        {
+            return a;
+        }
+
+        public void setA(int a)
+        {
+            this.a = a;
+        }
+
+        public int getB()
+        {
+            return b;
+        }
+
+        public void setB(int b)
+        {
+            this.b = b;
+        }
+
+        public Racional Multiplicar(Racional p)
+        {
+            int a = this.a * p.getA();
+            int b = this.b * p.getB();
+            return new Racional(a, b);
+        }
+        public Racional Dividir(Racional p)
+        {
+            int a = this.a * p.getB();
+            int b = this.b * p.getA();
+            return new Racional(a, b);
+        }
+
+
+    }
+
+
+}
